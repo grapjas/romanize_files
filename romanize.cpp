dbcc03405a5cee8c96277e76f3d84320efd7786a
grapjas <70775180+grapjas@users.noreply.github.com>
2020-09-28 19:57:39 +0200

    better textToRoman function

--- romanize.cpp
+++ romanize.cpp
@@ -118,76 +118,28 @@ bool isDigit(char a)
 	return (a >= '0' && a <= '9');
 }
 
+
+
 string numeralToRoman(int number)
 {
-	string roman {""};
-	while (number > 0)
-	{
-		if (number >= 1000)
-		{
-			roman += "M";
-			number -= 1000;
-		}
-		else if (number >= 900)
-		{
-			roman += "CM";
-			number -= 900;
-		}
-		else if (number >= 500)
-		{
-			roman += "D";
-			number -= 500;
-		}
-		else if (number >= 400)
-		{
-			roman += "CD";
-			number -= 400;
-		}
-		else if (number >= 100)
-		{
-			roman += "C";
-			number -= 100;
-		}
-		else if (number >= 90)
-		{
-			roman += "XC";
-			number -= 90;
-		}
-		else if (number >= 50)
-		{
-			roman += "L";
-			number -= 50;
-		}
-		else if (number >= 40)
-		{
-			roman += "XL";
-			number -= 40;
-		}
-		else if (number >= 10)
-		{
-			roman += "X";
-			number -= 10;
-		}
-		else if (number >= 9)
-		{
-			roman += "IX";
-			number -= 9;
-		}
-		else if (number >= 5)
-		{
-			roman += "V";
-			number -= 5;
-		}
-		else if (number >= 4)
-		{
-			roman += "IV";
-			number -= 4;
-		}
-		else if (number >= 1)
-		{
-			roman += "I";
-			number -= 1;
-		}
-	}
-	return roman;
+	const vector <string> romans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+	const vector <int> numerals = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    string roman;
+
+    int i = 0;
+
+    while (number > 0 && i < numerals.size())
+    {
+        if (number >= numerals[i])
+        {
+            roman += romans[i];
+            number -= numerals[i];
+        }
+        else
+        {
+            i++;
+        }
+    }
+
+    return roman;
 }
